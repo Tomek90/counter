beab45105a1a8a22d87301b5f3750c88c0791d3c
Tomek90 <tomaszgryczka90@gmail.com>
2024-01-20 19:31:25 +0100

    create counter react simple app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [state, setState] = useState(0)
+  const [inputNumber, setInputNumber] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex justify-content-center align-items-center bg-dark" style={{ minHeight: "100vh" }}>
+      <div className='container bg-white py-4'>
+        <div className="row mb-5 mt-5 justify-content-center text-center">
+          <h2>Counter</h2>
+        </div>
+        <div className="row justify-content-center my-2 mb-5">
+          <div className='col-1  text-end'>
+            <button className='btn btn-danger' onClick={() => setState(state-1)}>-</button>
+          </div>
+          <div className='col-2'>
+            <input type='number' className='form-control' value={state} readOnly></input>
+          </div>
+          <div className='col-1'>
+            <button className='btn btn-success' onClick={() => setState(state+1)}>+</button>
+          </div>
+        </div>
+        <div className='row justify-content-center my-2 mb-5'>
+          <div className='col-2'>
+            <input type='number' className='form-control' onChange={(e) => setInputNumber(Number(e.target.value))}></input>
+          </div>
+          <div className='col-1'>
+            <button className='btn btn-primary' onClick={() => setState(state + inputNumber)}>Add</button>
+          </div>
+          <div className='col-1'>
+            <button className='btn btn-secondary' onClick={() => setState(state - inputNumber)}>Deduct</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
